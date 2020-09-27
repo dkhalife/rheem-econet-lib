a74496c94f4edd9be512ddd661dd541fa1348a9f
Dany Khalife <dev@dkhalife.com>
2020-09-27 00:24:31 -0700

    Fix module exports

--- src/config/user.local.ts
+++ src/config/user.local.ts
@@ -1,6 +1,6 @@
 // Edit the values below but do not commit this file
 
-export const user = {
+module.exports = {
     "email": "FAKE_EMAIL_DO_NOT_COMMIT",
     "password": "FAKE_PASSWORD_DO_NOT_COMMIT",
 }
