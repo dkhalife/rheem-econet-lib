ea3c9b570893bd74c1c09d829e74209c2ae250d2
Dany Khalife <dev@dkhalife.com>
2020-09-27 00:23:37 -0700

    Fixup constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,7 +5,7 @@ export const REST_API_URL = `${HOST_URL}/api/v/1`;
 export const CLEAR_BLADE_SYSTEM_KEY = "e2e699cb0bb0bbb88fc8858cb5a401";
 export const CLEAR_BLADE_SYSTEM_SECRET = "E2E699CB0BE6C6FADDB1B0BC9A20";
 
-export const HEADERS = {
+export const CLEARBLADE_HEADERS = {
     "ClearBlade-SystemKey": CLEAR_BLADE_SYSTEM_KEY,
     "ClearBlade-SystemSecret": CLEAR_BLADE_SYSTEM_SECRET,
     "Content-Type": "application/json; charset=UTF-8"
--- src/config/user.ts
+++ src/config/user.ts
@@ -1,23 +1,15 @@
 const env_email = process.env.RHEEM_ECONET_EMAIL || null;
 const env_password = process.env.RHEEM_ECONET_PASSWORD || null;
 
-const local = require('./user.local')
+const local_user = require('./user.local')
 
-export const user = (() => {
-    if (env_email && env_password) {
-        return {
-            "email": env_email,
-            "password": env_password,
-        }
+if (env_email && env_password) {
+    module.exports = {
+        "email": env_email,
+        "password": env_password,
     }
-
-    if (local) {
-        const local_user = local.user;
-
-        if (local_user.email && local_user.password) {
-            return local_user
-        }
-    }
-
+} else if (local_user && local_user.email && local_user.password) {
+    module.exports = local_user
+} else {
     throw new Error("Unable to detect user email and password. Did you forget to edit the user.local.ts file?");
-})()
+}
